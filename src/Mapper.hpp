3ffef3f6ef7dfa3c758ea409862f4877e27a7a87
Rayane Ayoub AIT ALLAOUA <ayoubaitallaoua@gmail.com>
2025-03-24 11:29:40 +0100

    correction of errors, getSequences and getReads methods added

--- src/Mapper.hpp
+++ src/Mapper.hpp
@@ -4,20 +4,22 @@
 #include "KmerIndex.hpp"
 #include "Sequence.hpp"
 #include <vector>
+#include <unordered_map>
 
 class Mapper {
 public:
-    Mapper(int k);
+    Mapper(int k, int min_hits);
     void loadReference(const std::string& filename);
-    void loadReads(const std::string& filename, bool isFastq);
+    void loadReadsFromDirectory(const std::string& dirPath);
     void mapReads();
     void printMappings() const;
 
 private:
-    int k;
-    KmerIndex genomeIndex;
-    std::vector<Sequence> reads;
-    std::unordered_map<std::string, std::vector<int>> mappings;
+    int k;  // Taille des k-mers
+    int min_hits; // Seuil minimum de hits pour considérer un read comme mappé
+    KmerIndex genomeIndex;  // Index des k-mers du génome
+    std::vector<Sequence> reads;  // Reads valides
+    std::unordered_map<std::string, std::vector<int>> mappings;  // Stockage des résultats du mapping
 };
 
 #endif
